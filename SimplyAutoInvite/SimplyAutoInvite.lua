f62cf251d041838d635e41f552a2870aa68270a1
June <khazoda@gmail.com>
2022-11-10 01:16:33 +0000

    Addon fully preliminarily functional

--- SimplyAutoInvite/SimplyAutoInvite.lua
+++ SimplyAutoInvite/SimplyAutoInvite.lua
@@ -8,7 +8,9 @@ saiDB = {
 keywordDB = {}
 local versionNumber = GetAddOnMetadata("SimplyAutoInvite", "Version")
 local saimsgPrefix = "|cFFFF6B68<|r|cFFFF4CA9SAI|r|cFFFF6B68>|r "
+local saimsgCurio = "|cFFFF6B68›|r "
 
+local clearAwaitingConfirmation = false
 local GuildInvite = GuildInvite
 local InviteUnit = C_PartyInfo.InviteUnit
 local print = print
@@ -51,8 +53,14 @@ function sai:CHAT_MSG_GUILD(msg, charname, _)
     return
 end
 
+-- Helper functions
 function concatPrefix(s) return (s:gsub("%b[] ", "")) end
 
+local function has_value(tab, val)
+    for index, value in ipairs(tab) do if value == val then return true end end
+
+    return false
+end
 _M.handleGuildMsg = function(msg, charname, outgoing)
     _M.process_msg(msg, charname, outgoing)
 end
@@ -98,23 +106,26 @@ _M.printInfo = function(subject)
     if (subject == "") then
         print(
             "|cFFFF6B68<|r|cFFFF4CA9SimplyAutoInvite|r|cFFFF6B68>|r  Instructions:|r")
-        print(
-            "|cFFFF6B68›|r ------------------------------------------------------------------")
-        print(
-            "|cFFFF6B68›|r Create inviting keyword: |cFF5EFF56/sai add [keyword]|r")
-        print(
-            "|cFFFF6B68›|r Delete inviting keyword: |cFFFF5956/sai remove [keyword]|r")
-        print("|cFFFF6B68›|r ")
-        print("|cFFFF6B68›|r If anyone types your keyword in the guild chat,")
-        print(
-            "|cFFFF6B68›|r your client will automatically invite them to your group.")
-        print(
-            "|cFFFF6B68›|r ------------------------------------------------------------------")
+        print(saimsgCurio ..
+                  "------------------------------------------------------------------")
+        print(saimsgCurio ..
+                  "Create inviting keyword: |cFF5EFF56/sai add [keyword]|r")
+        print(saimsgCurio ..
+                  "Delete inviting keyword: |cFFFF5956/sai remove [keyword index]|r")
+        print(saimsgCurio)
+        print(saimsgCurio .. "If anyone types your keyword in the guild chat,")
+        print(saimsgCurio ..
+                  "your client will automatically invite them to your group.")
+        print(saimsgCurio .. "")
+        print(saimsgCurio ..
+                  "To see your active keywords, type |cFFFFCF56/sai list|r")
+        print(saimsgCurio ..
+                  "------------------------------------------------------------------")
     else
         print(saimsgPrefix .. "|cFFFF4343/sai " .. subject ..
                   " is not a valid command.|r")
-        print(
-            "|cFFFF6B68›|r |cFFFFCF56Please try '/sai help' for a list of commands.|r")
+        print(saimsgCurio ..
+                  "Please try |cFFFFCF56'/sai help'|r for a list of commands.")
     end
 end
 
@@ -124,57 +135,96 @@ _M.alterList = function(keyword, add)
     if (keyword == nil) then error = true end
     if (error) then
         if (add) then
-            print(saimsgPrefix .. "|cFFFF4343Incorrect usage. Try:|r")
-            print(
-                "|cFFFF6B68›|r |cFFFFCF56'/sai add [new keyword]' (no brackets)|r")
+
+            print(saimsgPrefix .. "|cFFFF4343Incorrect usage.|r Try:")
+            print(saimsgCurio ..
+                      "|cFFFFCF56'/sai add [new keyword]' (no brackets)|r")
         else
-            print(saimsgPrefix .. "|cFFFF4343Incorrect usage. Try:|r")
-            print(
-                "|cFFFF6B68›|r |cFFFFCF56'/sai remove [keyword]' (no brackets)|r")
+            print(saimsgPrefix .. "|cFFFF4343Incorrect usage.|r Try:")
+            print(saimsgCurio ..
+                      "|cFFFFCF56'/sai remove [keyword index]' (no brackets)|r")
         end
         return
 
     else
-        table.insert(keywordDB, keyword)
         if (add) then
-            print(saimsgPrefix .. "|cFF5EFF56Added|r |cFFFFCF56'" .. keyword ..
-                      "' |r|cFF5EFF56to your keywords|r")
+            if (has_value(keywordDB, keyword)) then
+                print(saimsgPrefix .. "|cFFFF4343Keyword '" .. keyword ..
+                          "' is already active.|r")
+            else
+                table.insert(keywordDB, keyword)
+                print(
+                    saimsgPrefix .. "|cFF5EFF56Added|r |cFFFFCF56'" .. keyword ..
+                        "' |r|cFF5EFF56to your keywords|r")
+            end
         else
-            print(
-                saimsgPrefix .. "|cFFFF5956Removed|r |cFFFFCF56'" .. keyword ..
-                    "' |r|cFFFF5956from your keywords|r")
+            -- Removes keyword at given numerical index
+            local index = tonumber(keyword)
+            if (type(index) == "number" and index <= #keywordDB) then
+                local removed_keyword = keywordDB[index]
+
+                table.remove(keywordDB, index)
+                print(saimsgPrefix .. "|cFFFF5956Removed|r |cFFFFCF56'" ..
+                          removed_keyword ..
+                          "' |r|cFFFF5956from your keywords|r")
+            else
+                print(saimsgPrefix ..
+                          '|cFFFF4343Please enter a valid index. Type |r|cFFFFCF56/sai list |cFFFF4343to see keyword indices|r')
+            end
+
         end
     end
 
 end
 
+_M.clearList = function()
+    print(saimsgPrefix ..
+              "|cFFFF5956WARNING:|r Are you sure you want to clear your list of keywords?")
+    print(saimsgCurio .. "If you are, please type  |cFFFFCF56/sai confirm|r")
+    clearAwaitingConfirmation = true
+end
+
+_M.confirmClear = function()
+    if (clearAwaitingConfirmation) then
+        keywordDB = {}
+        print(saimsgPrefix .. "|cFF009DFFYour keywords have been cleared.")
+    end
+end
+
 _M.listList = function()
-    print("List of active keywords:")
-    -- print(saiDB['inv'])
-
-    local people = {
-        {name = "Fred", address = "16 Long Street", phone = "123456"},
-        {name = "Wilma", address = "16 Long Street", phone = "123456"},
-        {name = "Barney", address = "17 Long Street", phone = "123457"}
-    }
-
-    for index, data in ipairs(keywordDB) do print(index, data) end
+    print(saimsgPrefix .. "List of active keywords:")
+    print(saimsgCurio .. "-----------------------------------")
+    for index, data in ipairs(keywordDB) do
+        if (index % 2 == 0) then
+            print(saimsgCurio .. "|cFFFF8BD2" .. index .. " - " .. data .. "|r")
+        else
+            print(saimsgCurio .. "|cFFFF8B68" .. index .. " - " .. data .. "|r")
+        end
+    end
 end
 
 SLASH_sai1 = "/sai"
 SlashCmdList["sai"] = function(msg)
     local a1, a2 = strsplit(" ", strlower(msg), 2)
-    if (a1 == "") then
-        _M.printInfo("")
-    elseif (a1 == "help") then
-        _M.printInfo("")
-    elseif (a1 == "add") then
-        _M.alterList(a2, true)
-    elseif (a1 == "remove") then
-        _M.alterList(a2, false)
-    elseif (a1 == "list") then
-        _M.listList()
+    if (a1 == "confirm") then
+        _M.confirmClear()
     else
-        _M.printInfo(a1)
+        clearAwaitingConfirmation = false
+        if (a1 == "") then
+            _M.printInfo("")
+        elseif (a1 == "help") then
+            _M.printInfo("")
+        elseif (a1 == "add") then
+            _M.alterList(a2, true)
+        elseif (a1 == "remove") then
+            _M.alterList(a2, false)
+        elseif (a1 == "clear") then
+            _M.clearList()
+        elseif (a1 == "list") then
+            _M.listList()
+        else
+            _M.printInfo(a1)
+        end
     end
 end
+
